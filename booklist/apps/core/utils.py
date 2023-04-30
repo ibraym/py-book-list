69a3464294d9f5580d89a4a5b0d3788fc7e8bb1c
Ibrahem Mouhamad <ibrahem.y.mouhamad@gmail.com>
2023-04-30 15:18:13 +0700

    Get column names dynamically

--- booklist/apps/core/utils.py
+++ booklist/apps/core/utils.py
@@ -2,5 +2,11 @@
 #
 # SPDX-License-Identifier: MIT
 
+from booklist.apps.core.models import Book
+
 def get_book_column_names():
-    return ['name', 'title', 'author', 'description', 'price']
+    column_name = []
+    for field in Book._meta.get_fields():
+        if field.name != 'id':
+            column_name.append(field.name)
+    return column_name
